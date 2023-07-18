dba067ebfeb461b083bf606d02929848deb0835c
Jonathan S. Rouach <jon@arjeo.com>
2023-07-18 10:12:50 +0200

    Fix docker base build

--- scripts/build_backend_docker_base_image.sh
+++ scripts/build_backend_docker_base_image.sh
@@ -1,2 +1,26 @@
-docker build -t arjeo/talk-to-repo-backend-base -f ../backend/Dockerfile.base ../backend
-docker push arjeo/talk-to-repo-backend-base
+#!/bin/bash
+#
+# Build the base Docker image for the backend service and push it to the Docker registry
+
+# Set Docker registry user
+DOCKER_REGISTRY_USER=arjeo
+
+# Check if user is logged in to Docker registry
+if ! docker info | grep -q "${DOCKER_REGISTRY_USER}"; then
+  echo "Error: Not logged in to Docker registry as ${DOCKER_REGISTRY_USER}"
+  exit 1
+fi
+
+# Build Docker image
+if ! docker build -t ${DOCKER_REGISTRY_USER}/talk-to-repo-backend-base -f ../backend/Dockerfile.base ../backend; then
+  echo "Error: Docker build failed"
+  exit 1
+fi
+
+# Push Docker image to registry
+if ! docker push ${DOCKER_REGISTRY_USER}/talk-to-repo-backend-base; then
+  echo "Error: Docker push failed"
+  exit 1
+fi
+
+echo "Docker image built and pushed successfully"
